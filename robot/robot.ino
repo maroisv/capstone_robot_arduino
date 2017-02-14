d5fb15dddf4591d4e1c29b7dc367d2e3ef34a099
Vincent Marois <vincent.marois5@gmail.com>
2017-02-14 00:08:53 -0500

    Adds imple object avoidance.

--- robot/robot.ino
+++ robot/robot.ino
@@ -3,6 +3,7 @@
 
 Control control(200);
 Sensors sensors;
+boolean autonomous = false;
 
 void setup(void)
 {
@@ -14,44 +15,64 @@ void setup(void)
 
 void loop(void)
 {
-  while (Serial.available() < 1) {} // Wait until a character is received
-  char val = Serial.read();
-  switch(val) // Perform an action depending on the command
-  {
-    case 'w'://Move Forward
-    case 'W':
+  if (Serial.available() > 0) {
+    char val = Serial.read();
+    switch(val) // Perform an action depending on the command
+    {
+      case 'w'://Move Forward
+      case 'W':
+        control.forward();
+        break;
+      case 's'://Move Backwards
+      case 'S':
+        control.reverse();
+        break;
+      case 'a'://Turn Left
+      case 'A':
+        control.left();
+        break;
+      case 'd'://Turn Right
+      case 'D':
+        control.right();
+        break;
+      case 't': // Temperature
+      case 'T':
+        Serial.println(sensors.getTemperature());
+        break;
+      case 'e':
+      case 'E':
+        Serial.println(sensors.getDistance(0));
+        Serial.println(sensors.getDistance(1));
+        Serial.println(sensors.getDistance(2));
+        break;
+      case 'g': // gas level
+      case 'G':
+        Serial.println(sensors.getGasLevel());
+        break;
+      case 'o': // sound level
+      case 'O':
+        Serial.println(sensors.getSoundLevel());
+        break;
+      case 'u': // sound level
+      case 'U':
+        autonomous = true;
+        break;
+      default:
+        control.stop();
+        autonomous = false;
+        break;
+    }
+  }
+
+  if (autonomous) {
+    // Move forward or turn
+    if (sensors.getDistance(1) > 10) {
       control.forward();
-      break;
-    case 's'://Move Backwards
-    case 'S':
-      control.reverse();
-      break;
-    case 'a'://Turn Left
-    case 'A':
-      control.left();
-      break;
-    case 'd'://Turn Right
-    case 'D':
+    } 
+    else {
       control.right();
-      break;
-    case 't': // Temperature
-    case 'T':
-      Serial.println(sensors.getTemperature());
-      break;
-    case 'e':
-    case 'E':
-      Serial.println(sensors.getDistance(1));
-      break;
-    case 'g': // gas level
-    case 'G':
-      Serial.println(sensors.getGasLevel());
-      break;
-    case 'o': // sound level
-    case 'O':
-      Serial.println(sensors.getSoundLevel());
-      break;
-    default:
+      delay(500);
       control.stop();
-      break;
+    }
   }
 }
