b67ae691fa9736454663e76d00499f2d31e4de2d
Vincent Marois <vincent.marois5@gmail.com>
2017-04-02 23:09:27 -0400

    First version of the PD motor control.
    Proportional control for the distance and angle turned implemented, but needs to be optimized.
    PD control to enforce a straight line (stabilize both motor speed to the same value) implemented, but needs to be optimized.

--- robot/Control.h
+++ robot/Control.h
@@ -12,19 +12,28 @@ class Control
     Control();
     void initialize(int speed);
     void forward();
-    void forward(int speed);
+    void forward(int distance);
 	  void reverse();
 	  void left();
 	  void right();
+    void turn(int angleToRot);
     void stop();
     void beep(int len);
     void beepBeep(int period, int len);
     void debug_printEncoderCount();
     void resetEncoderCount();
+    int getSpeed();
+    void setSpeed(int speed);
   private:
     int _speed;
     static volatile int _encoderCountLeft;
     static volatile int _encoderCountRight;
+
+    float getTrackDistance(int num);
+    float getDistanceTravelled();
+    float getAngleRotation();
+    void correctSpeed(int timeDiff);
+    void debug_printEncoderSpeedInfo();
 
     static void incrementEncoderLeft();
     static void incrementEncoderRight();
